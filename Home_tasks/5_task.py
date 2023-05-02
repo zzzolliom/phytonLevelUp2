09a87058d8c5b43628846e5a2f5e07287f0dea4e
zzzolliom <zubolga419@gmail.com>
2023-05-02 10:22:24 +0300

    исправила 5-е задание

--- Home_tasks/5_task.py
+++ Home_tasks/5_task.py
@@ -2,8 +2,13 @@
 # При этом использовать только базовые операции, цикл for, функции и контейнерные типы данных.
 
 input_str = input("Введите любую строку:")
+punctuation = ".,:;-?!()[]{}<>/\|"
+without_punctuation=""
+for i in input_str:
+    if i not in punctuation:
+        without_punctuation+=i
+print(without_punctuation)
+lst_without_punctuation = without_punctuation.split()
 
-lst_from_input = input_str.split()
-
-uniq_worlds=set(lst_from_input)
+uniq_worlds=set(lst_without_punctuation)
 print(uniq_worlds)
